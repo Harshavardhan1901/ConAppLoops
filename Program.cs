958f55d5d47e80507d52d90c6d114faf88304d1a
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-07-12 09:36:47 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i=1; i <= 100;i++)
+            //{
+            //    if (i % 5 == 0) { break; }
+            //    Console.WriteLine(i+"\t");
+            //}
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0) { continue; }
+            //    Console.WriteLine(i + "\t");
+            //}
+
+            int num;
+            string choice;
+            do
+            {
+                Console.WriteLine("Enter Number to find out table of the Number");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Table of {num} as follows");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{num}*{i} = \t {(num * i)}");
+                }
+                Console.WriteLine("Do you wanna learn more \n If yes press y \n to exit press other key");
+                choice = Console.ReadLine().ToLower();
+            }
+            while (choice == "y");
+        }
+    }
+}
